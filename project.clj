c7f214eae8a85712d4f60f6e05b3656f662ef15f
Nicholas Kariniemi <nicholas.kariniemi@gmail.com>
2013-07-22 23:36:38 +0300

    Update clojure, webdriver versions

--- project.clj
+++ project.clj
@@ -3,8 +3,8 @@
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.4.0"] 
-                 [clj-webdriver "0.6.0-alpha7"]
+  :dependencies [[org.clojure/clojure "1.5.1"] 
+                 [clj-webdriver "0.6.0"]
                  [org.apache.pdfbox/pdfbox "1.6.0"]
                  [clojure-csv/clojure-csv "2.0.0-alpha1"]
                  [enlive "1.0.0"]]
