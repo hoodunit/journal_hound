04a72e0470e5ba128026680f640d6bdef5692cf5
Nicholas Kariniemi <nicholas.kariniemi@gmail.com>
2015-07-12 09:13:11 +0300

    Update deps: pdfbox, clojure-csv, enlive

--- project.clj
+++ project.clj
@@ -5,8 +5,8 @@
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.5.1"] 
                  [clj-webdriver "0.6.1"]
-                 [org.apache.pdfbox/pdfbox "1.6.0"]
-                 [clojure-csv/clojure-csv "2.0.0-alpha1"]
-                 [enlive "1.0.0"]
+                 [org.apache.pdfbox/pdfbox "1.8.9"]
+                 [clojure-csv/clojure-csv "2.0.1"]
+                 [enlive "1.1.5"]
                  [clj-http "1.1.2"]]
   :main journal-hound.core)
